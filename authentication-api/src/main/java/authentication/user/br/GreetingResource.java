85a5839185b8f0768c1884578c154afc14d0e0b8
Aldir Lyra <aldir_lyra@hotmail.com>
2022-11-09 23:31:07 -0300

    first commit

---
+++ authentication-api/src/main/java/authentication/user/br/GreetingResource.java
@@ -0,0 +1,38 @@
+package authentication.user.br;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.security.Authenticated;
+
+@Path("/hello")
+@Authenticated
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("aluno")
+    @RolesAllowed({"aluno", "admin"})
+    public String helloAluno() {
+        return "Olá aluno!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("professor")
+    @RolesAllowed({"professor", "admin"})
+    public String helloProfessor() {
+        return "Olá professor!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String helloAdmin() {
+        return "Olá admin!";
+    }
+}
